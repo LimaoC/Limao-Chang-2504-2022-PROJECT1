0b19f2eaeeaf2ebeb1ade03cb9ad08d27de3bd21
LimaoC <80520563+LimaoC@users.noreply.github.com>
2022-09-18 17:15:03 +1000

    feat: add PolynomialModP file (oops)

---
+++ src/polynomial_mod_p.jl
@@ -0,0 +1,45 @@
+#############################################################################
+#############################################################################
+#
+# This file defines the PolynomialModP type with several operations 
+#                                                                               
+#############################################################################
+#############################################################################
+
+########################################
+# PolynomialModP type and construction #
+########################################
+
+"""
+A PolynomialModP type - holds a Polynomial and a prime number.
+"""
+struct PolynomialModP <: Polynomial
+    polynomial::PolynomialSparse
+    prime::Int
+
+    # Inner constructor of PolynomialModP
+    function PolynomialModP(p::PolynomialSparse, prime::Int)
+        return new(p, prime)
+    end
+end
+
+###########
+# Display #
+###########
+function show(io::IO, p::PolynomialModP)
+    show(io, p.polynomial)
+end
+
+################################
+# Pushing and popping of terms #
+################################
+
+"""
+Push a new term into the polynomial.
+"""
+push!(p::PolynomialModP, t::Term) = push!(p.polynomial, t)
+
+"""
+Pop the leading term out of the polynomial.
+"""
+pop!(p::PolynomialModP)::Term = pop!(p.polynomial)
